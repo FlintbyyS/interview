ff3e3ddfd6b6495c1daf82c94f368b5515f67f8b
FlintbyyS <muzikbaev2002@mail.ru>
2023-05-16 19:57:46 +0700

    add validation for entities

--- src/main/java/ru/flint/interview/util/validation/ValidationUtil.java
+++ src/main/java/ru/flint/interview/util/validation/ValidationUtil.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.NotNull;
 import lombok.experimental.UtilityClass;
 import org.springframework.core.NestedExceptionUtils;
 import org.springframework.lang.NonNull;
+import ru.flint.interview.entity.AbstractEntity;
 import ru.flint.interview.util.exceptions.IllegalRequestDataException;
 import ru.flint.interview.web.dto.AbstractDTO;
 
@@ -12,6 +13,11 @@ import java.util.Optional;
 @UtilityClass
 public class ValidationUtil {
     public static void checkNew(@NotNull AbstractDTO bean) {
+        if (!bean.isNew()) {
+            throw new IllegalRequestDataException(bean.getClass().getSimpleName() + " must be new (id=null)");
+        }
+    }
+    public static void checkNew(@NotNull AbstractEntity bean) {
         if (!bean.isNew()) {
             throw new IllegalRequestDataException(bean.getClass().getSimpleName() + " must be new (id=null)");
         }
