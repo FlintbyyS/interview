6f07460c0d0c0c8ec7c99aada30dcb3f6271b803
FlintbyyS <muzikbaev2002@mail.ru>
2023-06-01 16:08:38 +0700

    fix security

--- src/main/java/ru/flint/interview/config/SecurityConfiguration.java
+++ src/main/java/ru/flint/interview/config/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -59,7 +60,7 @@ public class SecurityConfiguration {
                 .anyRequest().permitAll()
 //                .and().httpBasic()
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().csrf().disable();
+                .and().csrf().disable().cors(AbstractHttpConfigurer::disable);
         return http.build();
     }
 }
