04719b33c9817c7cb831c03c170ff0b8039754f8
FlintbyyS <muzikbaev2002@mail.ru>
2023-08-07 21:42:27 +0700

    Improve code quality

--- src/main/java/ru/flint/interview/entity/user/Role.java
+++ src/main/java/ru/flint/interview/entity/user/Role.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+@Getter
 @RequiredArgsConstructor
 public enum Role {
 
@@ -34,7 +35,6 @@ public enum Role {
             )
     );
 
-    @Getter
     private final Set<Permission> permissions;
 
     public List<SimpleGrantedAuthority> getAuthorities() {
